dbd8c2cd79aecdb56e3903e026933fe1566adef1
Aveek Chakraborty <aveek.github@gmail.com>
2024-04-09 22:38:49 +0530

    backend commit

--- app/api/success/route.ts
+++ app/api/success/route.ts
@@ -1,26 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
-import { db } from '@/database/db'
-import { master } from '@/database/schema'
-import { eq } from 'drizzle-orm';
-import { request } from "http";
-
-
-
 const supabaseUrl = process.env.S_URL || "";
 const supabaseAnonKey = process.env.A_KEY || "";
-
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-
 export async function GET(req: NextRequest) {
     const searchParams = req.nextUrl.searchParams;
     const query = searchParams.get('q');
-    
     const qdc = await supabase.from("master").select("qrcodedata").eq("unique_code", query).single();
-
-    return NextResponse.json({
-        "message": qdc.data?.qrcodedata
-    }
-    )
+    return NextResponse.json({"message": qdc.data?.qrcodedata})
 }
