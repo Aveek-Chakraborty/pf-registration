ba67e73ac0c94aba1dc9ec480ed5181aac1d230e
Aveek Chakraborty <aveek.github@gmail.com>
2024-04-13 01:46:00 +0530

    cacheing prob solved5

--- app/api/boyscert/route.ts
+++ app/api/boyscert/route.ts
@@ -1,7 +1,7 @@
 export const dynamic = "force-dynamic"
 export const revalidate = 0 
 export const fetchCache = "only-no-store"
-
+export const maxDuration = 10000
 
 
 import { NextResponse } from "next/server";
