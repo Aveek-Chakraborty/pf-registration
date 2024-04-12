fc241eca042747ad02dfa5b3a16b55d8d2de1e8a
Aveek Chakraborty <aveek.github@gmail.com>
2024-04-13 01:22:42 +0530

    cacheing prob solved2

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,9 @@ export const metadata: Metadata = {
   description: "Generated for saving Pathfinder's ass",
 };
 
+export const dynamic = "force-dynamic"
 export const revalidate = 0 
+export const fetchCache = "force-no-store"
 export default function RootLayout({
   children,
 }: Readonly<{
