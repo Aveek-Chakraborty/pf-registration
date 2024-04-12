b792523411abeb0f4628f220ddeb10902f35c6e4
Aveek Chakraborty <aveek.github@gmail.com>
2024-04-12 18:11:46 +0530

    QR scanner button added

--- app/qfLWNw4amesF7rS/admin/page.tsx
+++ app/qfLWNw4amesF7rS/admin/page.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Participate from '@/components/Participate'
 import Girlsparticipate from '@/components/Girlsparticipate'
 import Walkparticipate from '@/components/Walkparticipate'
+import Qrscanner from '@/components/Qrscanner'
 
 const page = () => {
   return (
@@ -11,6 +12,7 @@ const page = () => {
       <Participate/>
       <Girlsparticipate/>
       <Walkparticipate/>
+      <Qrscanner/>
     </div>
   )
 }
---
+++ components/Qrscanner.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import Link from 'next/link'
+
+const Qrscanner = () => {
+  return (
+    <div>
+      <Link href='https://pf-qr-scanner.web.app/'><button>QR SCanner</button></Link>
+    </div>
+  )
+}
+
+export default Qrscanner
