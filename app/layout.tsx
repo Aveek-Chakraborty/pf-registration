e6ba3585c06f2611a326e8f4b36153b7ee74d887
Aveek Chakraborty <aveek.github@gmail.com>
2024-04-13 01:27:06 +0530

    cacheing prob solved3

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 
 export const dynamic = "force-dynamic"
 export const revalidate = 0 
-export const fetchCache = "force-no-store"
+export const fetchCache = "only-no-store"
 export default function RootLayout({
   children,
 }: Readonly<{
