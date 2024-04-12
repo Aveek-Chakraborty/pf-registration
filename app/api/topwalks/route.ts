10574b5c40d22680dfad5261ef03d0092fee21e3
Aveek Chakraborty <aveek.github@gmail.com>
2024-04-12 18:31:47 +0530

    all top participants apis added

---
+++ app/api/topwalks/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js";
+
+
+
+const supabaseUrl = process.env.S_URL || "";
+const supabaseAnonKey = process.env.A_KEY || "";
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
+export async function GET(req: any) {
+    try {
+        const { data, error } = await supabase
+            .from('walkcross')
+            .select('name, time')
+            .order('time', { ascending: true })
+            .limit(10)
+
+        if (error) {
+            console.error("Unexpected error:", error);
+            return NextResponse.json({ message: "Internal server error" });
+        }
+
+        return NextResponse.json({ message: "Data fetched successfully", data });
+
+    } catch (error: any) {
+        console.error("Unexpected error:", error);
+        return NextResponse.json({ message: "Internal server error" });
+    }
+}
